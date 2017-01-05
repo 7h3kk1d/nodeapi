2f57421d2447f53d24262cf731af1301dc0762a1
Alexander Bandukwala <7h3kk1d@gmail.com>
2017-01-04 21:43:26 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,62 @@
+// Define column names first that way they can be used in the cli options
+const column_names = ["last", "first", "gender", "dob"]
+
+// Adds cli options
+var argv = require('yargs') // Adds cli options
+    .usage('Usage: $0 <input_file> [options]')
+    .demandCommand(1)
+    .alias('d', 'delimiter')
+    .option('sort-column', {
+        alias: 'c',
+        describe: 'Column to sort',
+        choices: column_names
+     })
+    .option('sort-direction', {
+        alias: 's',
+        describe: 'Direction of sort',
+        choices: ['ascending', 'descinding'] 
+     })
+    .default('d', ',')
+    .help('h')
+    .alias('h', 'help')
+    .argv;
+
+// Libraries
+var fs = require('fs');
+var csv = require('csv');
+var moment = require('moment');
+
+
+var records = []
+var parseOptions = { "delimiter" : argv['d'] }
+var parser = csv.parse(parseOptions);
+var input = fs.createReadStream(argv._[0]);
+
+// Sort an array of arrays by the column value
+function sortByColumn(columnIndex) {
+    return function(a, b) {
+        if(a[columnIndex] < b[columnIndex])
+            return -1;
+        if(a[columnIndex] > b[columnIndex])
+            return 1;
+        return 0;
+    }
+}
+
+// Convert dates to moment objects
+function parseRecord(rawRecord) {
+    rawRecord[4] = moment(rawRecord[4], "M/D/YYYY");
+    return rawRecord;
+}
+
+
+
+parser.on('data', (record) => {
+    records.push(parseRecord(record));
+});
+input.pipe(parser)
+
+parser.on('end', () => {
+    records.sort(sortByColumn(4));
+    console.log(records);
+});
