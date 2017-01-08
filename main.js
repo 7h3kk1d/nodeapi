9a1dd341a784b2d2d4fea2a45a1f7dd3d5697851
Alexander Bandukwala <7h3kk1d@gmail.com>
2017-01-08 14:55:38 -0600

    Step 1 complete

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Define column names first that way they can be used in the cli options
-const column_names = ["last", "first", "gender", "dob"]
+const columnNames = ["last", "first", "gender", "dob"]
+const dateFormat = "M/D/YYYY";
 
 // Adds cli options
 var argv = require('yargs') // Adds cli options
@@ -9,12 +10,14 @@ var argv = require('yargs') // Adds cli options
     .option('sort-column', {
         alias: 'c',
         describe: 'Column to sort',
-        choices: column_names
+        choices: columnNames,
+        array: true
      })
     .option('sort-direction', {
         alias: 's',
         describe: 'Direction of sort',
-        choices: ['ascending', 'descinding'] 
+        choices: ['ascending', 'descending'] ,
+        array: true
      })
     .default('d', ',')
     .help('h')
@@ -31,25 +34,50 @@ var moment = require('moment');
 var records = []
 
 // CSV Parsing
-var parseOptions = { "delimiter" : argv['d'] }
+var parseOptions = { "delimiter" : argv['d'], trim: true }
 var parser = csv.parse(parseOptions);
 var input = fs.createReadStream(argv._[0]);
 
-// Sort an array of arrays by the columns values
-function sortByColumn(columnIndices) {
-    return (a, b) => {
-        if(a[columnIndex] < b[columnIndex])
-            return -1;
-        if(a[columnIndex] > b[columnIndex])
+// Options
+var sortColumns = argv.c.map((columnName) => {
+    return columnNames.indexOf(columnName)
+});
+var sortDirections = argv.s.map((direction) => {
+    switch(direction) {
+        case 'ascending':
             return 1;
+        case 'descending':
+            return -1;
+    }
+});
+
+// Sort an array of arrays by the columns values
+function sortByColumns(columnIndices, columnDirections) {
+    return function(a, b) {
+        for(var i = 0; i < columnIndices.length; i++) {
+            if(a[i] < b[i])
+                return columnDirections[i] * -1;
+            if(a[i] > b[i])
+                return columnDirections[i];
+        }
         return 0;
     }
 }
 
 // Convert dates to moment objects
 function parseRecord(rawRecord) {
-    rawRecord[4] = moment(rawRecord[4], "M/D/YYYY");
+    rawRecord[4] = moment(rawRecord[4], dateFormat);
     return rawRecord;
+}
+
+// Display functions
+function displayRecord(record) {
+    outputRecord = record.slice()
+    outputRecord[4] = record[4].format(dateFormat);
+    console.log(outputRecord.join(argv.d));
+}
+function displayRecords(records) {
+    records.map(displayRecord);
 }
 
 // Push each row to the records object
@@ -59,6 +87,6 @@ parser.on('data', (record) => {
 input.pipe(parser)
 
 parser.on('end', () => {
-    records.sort(sortByColumn(4));
-    console.log(records);
+    records.sort(sortByColumns(sortColumns, sortDirections));
+    displayRecords(records);
 });
